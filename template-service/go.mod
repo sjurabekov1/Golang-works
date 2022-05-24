d87316f4a132e71d4371cb658d8dbf94f3485332
sjurabekov1 <sjurabekov1@gmail.com>
2022-05-24 19:46:01 +0500

    project

--- template-service/go.mod
+++ template-service/go.mod
@@ -1,3 +1,12 @@
 module github.com/sjurabekov1/homeworks/template-service
 
 go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.6
+	github.com/spf13/cast v1.5.0
+	go.uber.org/zap v1.21.0
+	google.golang.org/grpc v1.46.2
+)
--- template-service/service/user.go
+++ template-service/service/user.go
@@ -1,27 +1,28 @@
 package service
 
 import (
-    "context"
-    "github.com/jmoiron/sqlx"
-    pb "github.com/rustagram/template-service/genproto"
-    l "github.com/rustagram/template-service/pkg/logger"
-    "github.com/rustagram/template-service/storage"
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	pb "github.com/sjurabekov1/homeworks/template-service/genproto"
+	l "github.com/sjurabekov1/homeworks/template-service/pkg/logger"
+	"github.com/sjurabekov1/homeworks/template-service/storage"
 )
 
 //UserService ...
 type UserService struct {
-    storage storage.IStorage
-    logger  l.Logger
+	storage storage.IStorage
+	logger  l.Logger
 }
 
 //NewUserService ...
 func NewUserService(db *sqlx.DB, log l.Logger) *UserService {
-    return &UserService{
-        storage: storage.NewStoragePg(db),
-        logger:  log,
-    }
+	return &UserService{
+		storage: storage.NewStoragePg(db),
+		logger:  log,
+	}
 }
 
 func (s *UserService) Create(ctx context.Context, req *pb.User) (*pb.User, error) {
-    return nil, nil
+	return nil, nil
 }
